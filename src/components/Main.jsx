25f6027c723b8423a0e6e807336751a97658cdd3
Robert Servado <servadorobert@gmail.com>
2023-01-07 18:33:05 -0500

    Cleaned up files and added information

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "./Button";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button name={"LinkedIn"} />
+    </div>
+  );
 };
 
 export default Main;
