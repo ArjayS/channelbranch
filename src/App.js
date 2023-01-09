9404002a92819c8b80bf16cbacf2e2fd3026b180
Robert Servado <servadorobert@gmail.com>
2023-01-09 16:16:26 -0500

    Modified App.js and added the socials variable here and added more details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import "./App.css";
 import Main from "./components/Main";
 
 function App() {
+  const data = {
+    name: "Robert Servado",
+    image: "https://github.com/ArjayS.png",
+    bio: "Welcome to my small app that displays my social links!",
+    socials: [
+      {
+        id: 1,
+        site_name: "LinkedIN",
+        site_description: "Let's connect on LinkedIN!",
+        link: "https://www.linkedin.com/in/robertservado/",
+      },
+      {
+        id: 2,
+        site_name: "Github",
+        site_description: "Checkout my github profile!",
+        link: "https://github.com/ArjayS",
+      },
+      {
+        id: 3,
+        site_name: "Creddle",
+        site_description: "Please check out my resume!",
+        link: "https://resume.creddle.io/resume/6nua8y8tbin",
+      },
+    ],
+  };
+
   return (
     <div className="App">
       <header className="App-header">
