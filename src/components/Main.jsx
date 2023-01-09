438bdac74b518cc12890f168c6839f073b146ec5
Robert Servado <servadorobert@gmail.com>
2023-01-09 16:15:31 -0500

    Remove the social variable in this file and moved it to App.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import Button from "./Button";
 
-const socials = [
-  {
-    id: 1,
-    sitename: "LinkedIN",
-    link: "https://www.linkedin.com/in/robertservado/",
-  },
-  {
-    id: 2,
-    sitename: "Github",
-    link: "https://github.com/ArjayS",
-  },
-];
-
 const Main = () => {
   return (
     <div>
