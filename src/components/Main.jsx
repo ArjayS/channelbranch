f3e250ae09c0e34298beef29af5443323380af67
Robert Servado <servadorobert@gmail.com>
2023-01-09 18:09:20 -0500

    Test to see if props are obtained and if it is also rendering, and it does!

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Main = (props) => {
-  const {} = props;
-  return <div></div>;
+  const { name, image, bio } = props;
+  return (
+    <div>
+      <header>
+        <p>{name}</p>
+        <p>{image}</p>
+        <p>{bio}</p>
+      </header>
+    </div>
+  );
 };
 
 export default Main;
