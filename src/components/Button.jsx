46772d63d27b0e956f758de45b159afae9e133c2
Robert Servado <servadorobert@gmail.com>
2023-01-09 21:03:20 -0500

    Modified the Button.jsx to add the site logo props.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Button = (props) => {
-  const { id, site_name, site_description, site_link } = props;
+  const { id, site_name, site_description, site_link, site_logo } = props;
   return (
     <form>
       <button id={id} type="submit" formAction={site_link}>
         {site_description}
+        <div>{site_name}</div>
       </button>
     </form>
   );
