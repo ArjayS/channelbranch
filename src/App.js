67a706f970d58003b4c07d33e3ae8d717b85a547
Robert Servado <servadorobert@gmail.com>
2023-01-09 21:01:44 -0500

    Modified App.js to include the site logo. I will likely use svg to display the logo.

--- src/App.js
+++ src/App.js
@@ -10,30 +10,35 @@ function App() {
       {
         id: 1,
         site_name: "LinkedIN",
+        site_logo: "",
         site_description: "Let's connect on LinkedIN!",
         link: "https://www.linkedin.com/in/robertservado/",
       },
       {
         id: 2,
         site_name: "Github",
+        site_logo: "",
         site_description: "Checkout my github profile!",
         link: "https://github.com/ArjayS",
       },
       {
         id: 3,
         site_name: "Discord",
+        site_logo: "",
         site_description: "Let's chat on Discord!",
         link: "https://discord.com/users/Robert Servado#9549",
       },
       {
         id: 4,
         site_name: "Gmail",
+        site_logo: "",
         site_description: "If you want to contact me via gmail",
         link: "servadorobert@gmail.com",
       },
       {
         id: 5,
         site_name: "Creddle",
+        site_logo: "",
         site_description: "Please check out my resume!",
         link: "https://resume.creddle.io/resume/6nua8y8tbin",
       },
@@ -49,6 +54,7 @@ function App() {
           site_name={social.site_name}
           site_description={social.site_description}
           site_link={social.link}
+          site_logo={social.site_logo}
         />
       ))}
     </div>
