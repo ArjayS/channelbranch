e8a154130e8ba0aba4cadc351bab0e5ff0f9190a
Robert Servado <servadorobert@gmail.com>
2023-01-11 16:46:23 -0500

    Modified Main.jsx and change the tag on my name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ const Main = (props) => {
   const { name, image, bio } = props;
   return (
     <>
-      <div className="temp-img-comp-wrapper mt-32">
+      <header className="temp-img-comp-wrapper mt-32">
         <div className="relative drop-shadow-2xl p-2">
           <div className="ring-2 ring-white rounded-full p-0.5">
             <img
@@ -18,13 +18,13 @@ const Main = (props) => {
             />
           </div>
         </div>
-        <p className="text-center text-2xl font-normal">
+        <h1 className="text-center text-2xl font-normal">
           <a target="_blank" href="https://github.com/ArjayS">
             {name}
           </a>
-        </p>
+        </h1>
         <p>{bio}</p>
-      </div>
+      </header>
     </>
   );
 };
