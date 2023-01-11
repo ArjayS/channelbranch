1f1e5232121e224d460e824c0291952920996274
Robert Servado <servadorobert@gmail.com>
2023-01-10 22:44:16 -0500

    Modified Main.jsx to have a proper design on the profile image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = (props) => {
         <p>{name}</p>
         <img
           src={image}
-          className="max-w-full h-auto"
+          className="image-placeholder w-32 h-32 rounded-full"
           alt="Robert Servado Github PNG Picture"
         />
         <p>{bio}</p>
