1bf6cc78030f1768337b5289f5a9631379816e95
Robert Servado <servadorobert@gmail.com>
2023-01-10 23:31:17 -0500

    Modified the Mainjsx profile picutre by adding a ring around it

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,19 @@ const Main = (props) => {
   const { name, image, bio } = props;
   return (
     <>
-      <header className="temp-img-comp-wrapper mt-32">
-        <div className="w-48 h-48 relative drop-shadow-2xl">
-          <img
-            layout="fill"
-            src={image}
-            className="rounded-full"
-            alt="Robert Servado Github PNG Picture"
-            objectfit="cover"
-          />
+      <div className="temp-img-comp-wrapper mt-32">
+        <div className="relative drop-shadow-2xl p-2">
+          <div className="ring-2 ring-white rounded-full p-0.5">
+            <img
+              layout="fill"
+              src={image}
+              height={350}
+              width={350}
+              className="rounded-full"
+              alt="Robert Servado Github PNG Picture"
+              objectfit="cover"
+            />
+          </div>
         </div>
         <p className="text-center text-2xl font-normal">
           <a target="_blank" href="https://github.com/ArjayS">
@@ -20,7 +24,7 @@ const Main = (props) => {
           </a>
         </p>
         <p>{bio}</p>
-      </header>
+      </div>
     </>
   );
 };
