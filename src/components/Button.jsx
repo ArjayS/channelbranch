9d8c566ccbd1f8ed731661d5f03122040cb66d88
Robert Servado <servadorobert@gmail.com>
2023-01-11 22:38:47 -0500

    Modified Button.jsx to add styling on each of the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Button = (props) => {
   const { id, site_name, site_description, site_link, site_logo } = props;
   return (
-    <form>
-      <button id={id} type="submit" formAction={site_link}>
-        {site_description}
-        <div>{site_name}</div>
-      </button>
-    </form>
+    <div className="bg-white rounded-full p-2 w-96 mt-3">
+      <form>
+        <button id={id} type="submit" formAction={site_link}>
+          {site_description}
+          <div>{site_name}</div>
+        </button>
+      </form>
+    </div>
   );
 };
 
