9721993f01dab15c0bf20b882aa4901660318798
Robert Servado <servadorobert@gmail.com>
2023-01-11 22:19:47 -0500

    Modified Main.jsx to add detail ring like style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,21 +7,23 @@ const Main = (props) => {
       {/* <header className="temp-img-comp-wrapper mt-32"> */}
       <header className="flex text-center flex-col items-center mt-5">
         {/* <div> */}
-        <div className="ring-2 ring-white rounded-full p-0.5">
+        <div className="ring-2 ring-white rounded-full p-1">
           {/* <div> */}
           <img
             src={image}
-            height={300}
-            width={300}
+            height={200}
+            width={200}
             className="rounded-full inline-block"
             alt="Robert Servado Github PNG Picture"
           />
         </div>
-        <h1 className="text-center text-2xl font-bold rounded-3xl bg-teal-800 text-white p-2 ring-1 ring-white mt-3">
-          <a target="_blank" href="https://github.com/ArjayS">
-            {name}
-          </a>
-        </h1>
+        <div className="ring-2 ring-white rounded-full p-0.5 mt-3">
+          <h1 className="text-center text-2xl font-bold rounded-3xl bg-teal-800 text-white p-2 ring-1 ring-white">
+            <a target="_blank" href="https://github.com/ArjayS">
+              {name}
+            </a>
+          </h1>
+        </div>
         <div className="mt-3">
           <p className="text-xl font-semibold">{bio}</p>
         </div>
