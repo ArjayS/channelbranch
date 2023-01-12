1d5b8545157f41ec996ce6a12891547ffc08db88
Robert Servado <servadorobert@gmail.com>
2023-01-11 21:52:36 -0500

    Modified Main.jsx to style my @

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,21 +5,19 @@ const Main = (props) => {
   return (
     <>
       {/* <header className="temp-img-comp-wrapper mt-32"> */}
-      <header className="mt-3 text-center">
-        {/* <div className="relative drop-shadow-2xl p-2"> */}
-        <div>
-          {/* <div className="ring-2 ring-white rounded-full p-0.5"> */}
-          <div>
-            <img
-              src={image}
-              height={350}
-              width={350}
-              className="rounded-full inline-block"
-              alt="Robert Servado Github PNG Picture"
-            />
-          </div>
+      <header className="flex text-center flex-col items-center mt-5">
+        {/* <div> */}
+        <div className="ring-2 ring-white rounded-full p-0.5">
+          {/* <div> */}
+          <img
+            src={image}
+            height={300}
+            width={300}
+            className="rounded-full inline-block"
+            alt="Robert Servado Github PNG Picture"
+          />
         </div>
-        <h1 className="text-center text-2xl font-normal">
+        <h1 className="text-center text-2xl font-bold rounded-3xl bg-teal-800 text-white p-2 ring-1 ring-white mt-3">
           <a target="_blank" href="https://github.com/ArjayS">
             {name}
           </a>
