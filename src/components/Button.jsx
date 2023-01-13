05b916867b2fc2763600a003f8f2df572b0e1428
Robert Servado <servadorobert@gmail.com>
2023-01-13 18:12:30 -0500

    Modiifed Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   const { id, site_name, site_link, site_logo } = props;
   return (
     <div className="cursor-pointer">
-      <form className="bg-white rounded-full flex p-3 mt-3 justify-around">
+      <form className="bg-white rounded-full flex p-3 mt-3 mx-3 justify-around">
         <button id={id} type="submit" formAction={site_link}>
           <img src={site_logo} alt={site_name} className=" w-10" />
         </button>
