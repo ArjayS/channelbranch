34542bae6ea9d3525099346a6c69fae455f6b603
Robert Servado <servadorobert@gmail.com>
2023-01-12 21:48:03 -0500

    Modified the Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ const Button = (props) => {
   const { id, site_name, site_description, site_link, site_logo } = props;
   return (
     <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
-      <form>
-        <p>{site_name}</p>
-        <img src={site_logo} alt={site_name} className="w-10" />
+      <form className="flex justify-around">
+        {/* <p>{site_name}</p> */}
+        <img src={site_logo} alt={site_name} className="ml-3 w-10" />
         <button id={id} type="submit" formAction={site_link}>
           {site_description}
         </button>
