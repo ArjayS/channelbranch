4f36249972cd3c6734257453ce9398515f979878
Robert Servado <servadorobert@gmail.com>
2023-01-12 19:05:05 -0500

    Modified Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,11 @@ const Button = (props) => {
   const { id, site_name, site_description, site_link, site_logo } = props;
   return (
     <div className="bg-white rounded-full p-2 w-96 mt-3">
+      <img src={site_logo} alt={site_name} />
       <form>
         <p>{site_name}</p>
         <button id={id} type="submit" formAction={site_link}>
           {site_description}
-          <img src={site_logo} alt={site_name} />
         </button>
       </form>
     </div>
