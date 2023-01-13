eef7e9001596d0e391381c5de6e781b4592d32a2
Robert Servado <servadorobert@gmail.com>
2023-01-12 22:10:31 -0500

    Added similar pattern from the current Button component to the Posts body

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,6 +5,15 @@ const Posts = (props) => {
   return;
   <>
     <h1>My Recent Posts!</h1>
+    <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+      <form className="flex justify-around">
+        <p></p>
+        <img src="" alt="" className="ml-3 w-10" />
+        <button id="" type="submit" formAction="">
+          ""
+        </button>
+      </form>
+    </div>
   </>;
 };
 
