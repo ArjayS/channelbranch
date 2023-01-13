02796831fe90d432fa4ff4acfa251e3e3e33f199
Robert Servado <servadorobert@gmail.com>
2023-01-13 18:01:49 -0500

    Modified the Button.jsx styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Button = (props) => {
-  const { id, site_name, site_description, site_link, site_logo } = props;
+  const { id, site_name, site_link, site_logo } = props;
   return (
-    <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+    <div className="bg-white rounded-full p-3 mt-3 cursor-pointer">
       <form className="flex justify-around">
         {/* <p>{site_name}</p> */}
-        <img src={site_logo} alt={site_name} className="ml-3 w-10" />
         <button id={id} type="submit" formAction={site_link}>
-          {site_description}
+          <img src={site_logo} alt={site_name} className=" w-10" />
         </button>
       </form>
     </div>
