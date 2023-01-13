7b48620a4b3f92ea6d9fc4eb2eafa8796a9070d8
Robert Servado <servadorobert@gmail.com>
2023-01-12 19:19:24 -0500

    Rearrange the location of the image logo and added a cursor pointer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Button = (props) => {
   const { id, site_name, site_description, site_link, site_logo } = props;
   return (
-    <div className="bg-white rounded-full p-2 w-96 mt-3">
-      <img src={site_logo} alt={site_name} />
+    <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
       <form>
         <p>{site_name}</p>
+        <img src={site_logo} alt={site_name} className="w-10" />
         <button id={id} type="submit" formAction={site_link}>
           {site_description}
         </button>
