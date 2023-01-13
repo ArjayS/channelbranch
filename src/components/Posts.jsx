6129a2c12f1b2b8d14ab17944bcc4a7acc00d761
Robert Servado <servadorobert@gmail.com>
2023-01-13 16:32:44 -0500

    Modified Posts.jsx to remove the h1 tag

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,6 @@ const Posts = (props) => {
   const {} = props;
   return;
   <>
-    <h1>My Recent Posts!</h1>
     <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
       <form className="flex justify-around">
         <p></p>
