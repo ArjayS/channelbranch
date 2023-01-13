6a1f88a2c3e2b8457ab540ccf475997dd2663159
Robert Servado <servadorobert@gmail.com>
2023-01-13 18:06:12 -0500

    Modified Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Button = (props) => {
   const { id, site_name, site_link, site_logo } = props;
   return (
-    <div className="bg-white rounded-full p-3 mt-3 cursor-pointer">
-      <form className="flex justify-around">
-        {/* <p>{site_name}</p> */}
+    <div className="cursor-pointer">
+      <form className="bg-white rounded-full flex p-3 mt-3 justify-around">
         <button id={id} type="submit" formAction={site_link}>
           <img src={site_logo} alt={site_name} className=" w-10" />
         </button>
       </form>
+      <p>{site_name}</p>
     </div>
   );
 };
