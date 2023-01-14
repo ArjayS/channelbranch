b19e6f09bf510953c6794a5b4f8e31d34debda07
Robert Servado <servadorobert@gmail.com>
2023-01-13 22:27:06 -0500

    Replaced the Main.jsx component to the HeaderProfile.jsx component

---
+++ src/components/HeaderProfile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Main = (props) => {
+  const { name, image, bio } = props;
+  return (
+    <>
+      <header className="flex text-center flex-col items-center h-50 mt-5">
+        <div className="ring-2 ring-white rounded-full p-1">
+          <img
+            src={image}
+            height={200}
+            width={200}
+            className="rounded-full inline-block"
+            alt="Robert Servado Github PNG Picture"
+          />
+        </div>
+        <div className="ring-2 ring-white rounded-full p-0.5 mt-3">
+          <h1 className="text-center text-2xl font-bold rounded-3xl bg-teal-800 text-white p-2 ring-1 ring-white">
+            <a target="_blank" href="https://github.com/ArjayS">
+              {name}
+            </a>
+          </h1>
+        </div>
+        <div className="mt-3">
+          <p className="text-xl font-semibold">{bio}</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Main;
