8b88b0eef8e74ddda06194d94614488b75613a30
Robert Servado <servadorobert@gmail.com>
2023-01-13 22:38:03 -0500

    Modified Socials.jsx by styling the social button

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,12 +3,19 @@ import React from "react";
 const Button = (props) => {
   const { id, site_name, site_link, site_logo } = props;
   return (
-    <div className="cursor-pointer">
-      <form className="bg-white rounded-full flex p-3 mt-3 mx-3 justify-around">
-        <button id={id} type="submit" formAction={site_link}>
-          <img src={site_logo} alt={site_name} className=" w-10" />
-        </button>
-      </form>
+    <div className="mt-2">
+      <div className="bg-white rounded-full p-3 m-3 cursor-pointer ">
+        <form className="flex justify-around">
+          <button
+            id={id}
+            type="submit"
+            formAction={site_link}
+            className="h-8 w-8"
+          >
+            <img src={site_logo} alt={site_name} className=" w-8" />
+          </button>
+        </form>
+      </div>
       <p>{site_name}</p>
     </div>
   );
