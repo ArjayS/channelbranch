bbb4bb4e999568c50f7ae0d923c624207ee022e0
Robert Servado <servadorobert@gmail.com>
2023-01-13 22:22:29 -0500

    Added the Socials.jsx component to the Button.jsx

---
+++ src/components/Socials.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = (props) => {
+  const { id, site_name, site_link, site_logo } = props;
+  return (
+    <div className="cursor-pointer">
+      <form className="bg-white rounded-full flex p-3 mt-3 mx-3 justify-around">
+        <button id={id} type="submit" formAction={site_link}>
+          <img src={site_logo} alt={site_name} className=" w-10" />
+        </button>
+      </form>
+      <p>{site_name}</p>
+    </div>
+  );
+};
+
+export default Button;
