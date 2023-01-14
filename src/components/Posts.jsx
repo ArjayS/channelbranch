1f9b54bd6d4db515838c474170149b93cb385b70
Robert Servado <servadorobert@gmail.com>
2023-01-13 23:34:50 -0500

    Modified Posts.jsx to simulate the idea of having 5 post in my app

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,18 +2,46 @@ import React from "react";
 
 const Posts = (props) => {
   const {} = props;
-  return;
-  <>
-    <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
-      <form className="flex justify-around">
-        <p></p>
-        <img src="" alt="" className="ml-3 w-10" />
-        <button id="" type="submit" formAction="">
-          ""
-        </button>
-      </form>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+        <form className="flex justify-around">
+          <p></p>
+          <img src="" alt="" className="ml-3 w-10" />
+          <button id="" type="submit" formAction="">
+            ""
+          </button>
+        </form>
+      </div>
+      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+        <form className="flex justify-around">
+          <p></p>
+          <img src="" alt="" className="ml-3 w-10" />
+          <button id="" type="submit" formAction="">
+            ""
+          </button>
+        </form>
+      </div>
+      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+        <form className="flex justify-around">
+          <p></p>
+          <img src="" alt="" className="ml-3 w-10" />
+          <button id="" type="submit" formAction="">
+            ""
+          </button>
+        </form>
+      </div>
+      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
+        <form className="flex justify-around">
+          <p></p>
+          <img src="" alt="" className="ml-3 w-10" />
+          <button id="" type="submit" formAction="">
+            ""
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Posts;
