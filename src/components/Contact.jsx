db2900efb932a903b2bbd99c152a035cc625c08d
Robert Servado <servadorobert@gmail.com>
2023-01-15 18:56:10 -0500

    Added a form to be able to submit to my email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Contact = () => {
-  return <></>;
+  return (
+    <>
+      <form
+        action="https://formsubmit.co/servadorobert@gmail.com"
+        method="POST"
+      >
+        <input type="text" name="name" required />
+        <input type="email" name="email" required />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
 };
 
 export default Contact;
