8f6aae17adbe661f123f99517425acd2a9c1ab7f
Robert Servado <servadorobert@gmail.com>
2023-01-17 17:35:48 -0500

    MOdified Posts.jsx to add proper hover on the copy url

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,25 +5,27 @@ const Posts = (props) => {
   return (
     <>
       {/*  */}
-      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-105 hover-trigger">
-        <div className="bg-slate-200 rounded-full p-1 ml-1 bg-opacity-25"></div>
-        <div className="flex justify-center w-9/12">
+      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-between items-center w-11/12 bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-110 hover-trigger">
+        <div className="bg-slate-200 rounded-full bg-opacity-25"></div>
+        <div className="flex justify-center">
           <h3 className="text-lg font-medium font-serif">Sample Post Title!</h3>
         </div>
-        <div className="bg-slate-200 rounded-full p-1 mr-1 bg-opacity-25 hover-target-fast">
-          <form className="flex justify-around">
-            <img
-              src={require("../imgs/Copy_logo3.svg").default}
-              alt="Copy Logo"
-              type="submit"
-              formAction=""
-              className="h-8 w-8"
-            />
-            {/* Might need to create a faster hover state on-show of copy-button and hide the dive that contains the backdrop currently seen here */}
-            <button id="" type="submit" formAction="">
-              {title}
-            </button>
-          </form>
+        <div className="rounded-full hover:bg-slate-200 hover:ring-2 mr-1">
+          <div className="rounded-full bg-opacity-75 hover-target-fast">
+            <form className="flex justify-around">
+              <img
+                src={require("../imgs/Copy_logo3.svg").default}
+                alt="Copy Logo"
+                type="submit"
+                formAction=""
+                className="h-4 w-4"
+              />
+              {/* Might need to create a faster hover state on-show of copy-button and hide the dive that contains the backdrop currently seen here */}
+              <button id="" type="submit" formAction="">
+                {title}
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
