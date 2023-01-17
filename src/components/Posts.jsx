3dfe2739a1fbc4ba17e96dcb02b065369d55c5f8
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:20:31 -0500

    Modified Posts.jsx to add a blurred background for new posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,7 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-evenly items-center w-full">
+      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-evenly items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md backdrop-saturate-150 backdrop-contrast-200 ring-neutral-100">
         <h3>Sample Post Title!</h3>
         <div className="bg-slate-200 rounded-full p-2 ">
           <form className="flex justify-around">
