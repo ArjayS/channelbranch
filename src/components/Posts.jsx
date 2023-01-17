d4f8cc1d5c5134e6ec8d9281164673f0059be8ad
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:29:34 -0500

    Modified Posts.jsx to style the text and the position of the copy button

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,9 +4,9 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-evenly items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md backdrop-saturate-150 backdrop-contrast-200 ring-neutral-100">
-        <h3>Sample Post Title!</h3>
-        <div className="bg-slate-200 rounded-full p-2 ">
+      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md backdrop-saturate-150 backdrop-contrast-200 ring-neutral-100">
+        <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
+        <div className="bg-slate-200 rounded-full p-2 mr-1">
           <form className="flex justify-around">
             <p></p>
             <img
