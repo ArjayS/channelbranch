a7da42890ea914cc04492bba73b37dc3c4e3c58c
Robert Servado <servadorobert@gmail.com>
2023-01-17 16:06:58 -0500

    Modified Posts.jsx to add another on-hover effect when viewing the reent post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,12 +5,12 @@ const Posts = (props) => {
   return (
     <>
       {/*  */}
-      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-125 hover-trigger">
-        <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25"></div>
-        <div className="flex justify-center w-10/12">
-          <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
+      <div className="bg-white rounded-full p-1 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-105 hover-trigger">
+        <div className="bg-slate-200 rounded-full p-1 ml-1 bg-opacity-25"></div>
+        <div className="flex justify-center w-9/12">
+          <h3 className="text-lg font-medium">Sample Post Title!</h3>
         </div>
-        <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25 hover-target-fast">
+        <div className="bg-slate-200 rounded-full p-1 mr-1 bg-opacity-25 hover-target-fast">
           <form className="flex justify-around">
             <img
               src={require("../imgs/Copy_logo3.svg").default}
