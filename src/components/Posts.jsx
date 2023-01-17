0f4bfe167a808c203110bbd2bf5d43da3ea97345
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:15:17 -0500

    Modiifed the share button styling

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,15 +4,15 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer flex flex-row justify-evenly items-center">
+      <div className="bg-white rounded-full p-1 w-96 mt-3 cursor-pointer flex flex-row justify-evenly items-center">
         <h3>Sample Post Title!</h3>
-        <div className="bg-slate-200 rounded-full">
+        <div className="bg-slate-200 rounded-full p-2">
           <form className="flex justify-around">
             <p></p>
             <img
               src={require("../imgs/Copy_logo1.svg").default}
               alt="Copy Logo"
-              className="w-8"
+              className="h-8 w-8"
             />
             <button id="" type="submit" formAction="">
               {title}
