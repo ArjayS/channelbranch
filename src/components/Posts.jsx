5ea7ccc63ad4c735b1380f688a85ca0335c3daa0
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:47:52 -0500

    Modified Psots.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,11 +5,8 @@ const Posts = (props) => {
   return (
     <>
       <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md">
-        <div className="flex w-10/12">
-          <h3 className="text-lg font-medium ml-4">
-            Sample Post
-            Title!sfewfwefgwagawegawegwaegwgewegwgewagewagewegwgwgwgwageawgewaegwagewagwgawegwegwagwag
-          </h3>
+        <div className="flex justify-center w-10/12">
+          <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
         </div>
         <div className="bg-slate-200 rounded-full p-2 mr-1">
           <form className="flex justify-around">
