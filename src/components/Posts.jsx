5c3eedbd8c6aeb8081e3ed357a60a89256472068
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:31:01 -0500

    Clean up the Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,7 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md backdrop-saturate-150 backdrop-contrast-200 ring-neutral-100">
+      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md backdrop-saturate-150 backdrop-contrast-200">
         <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
         <div className="bg-slate-200 rounded-full p-2 mr-1">
           <form className="flex justify-around">
