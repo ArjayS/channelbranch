cc242a06e55a4d840f5dc305430089f26ee5fbe7
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:00:14 -0500

    Modified Posts.jsx to have a bit of a design!

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,18 +4,21 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer">
-        <form className="flex justify-around">
-          <p></p>
-          <img
-            src={require("../imgs/Copy_logo1.svg").default}
-            alt="Copy Logo"
-            className="w-14"
-          />
-          <button id="" type="submit" formAction="">
-            {title}
-          </button>
-        </form>
+      <div className="bg-white rounded-full p-2 w-96 mt-3 cursor-pointer flex flex-row justify-evenly items-center">
+        <h3>Sample Post Title!</h3>
+        <div className="bg-slate-200 rounded-full">
+          <form className="flex justify-around">
+            <p></p>
+            <img
+              src={require("../imgs/Copy_logo1.svg").default}
+              alt="Copy Logo"
+              className="w-8"
+            />
+            <button id="" type="submit" formAction="">
+              {title}
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
