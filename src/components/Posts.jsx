3b2644fd30c1f7bc097e2339a3181c6339332fc3
Robert Servado <servadorobert@gmail.com>
2023-01-17 12:25:26 -0500

    Modified Posts.jsx to have the copy button show up on hover

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,7 +5,7 @@ const Posts = (props) => {
   return (
     <>
       {/*  */}
-      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-125">
+      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-125 hover-trigger">
         <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25"></div>
         <div className="flex justify-center w-10/12">
           <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
@@ -17,7 +17,7 @@ const Posts = (props) => {
               alt="Copy Logo"
               type="submit"
               formAction=""
-              className="h-8 w-8"
+              className="h-8 w-8 hover-target"
             />
             <button id="" type="submit" formAction="">
               {title}
