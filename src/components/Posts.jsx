6df6f31bb0eb3ae0fbf0543ba9be11d18cc71e3d
Robert Servado <servadorobert@gmail.com>
2023-01-17 12:23:42 -0500

    Modified Posts.jsx to have the post scale up on hover might have to change the scaling on other sections for better look later

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,7 +5,7 @@ const Posts = (props) => {
   return (
     <>
       {/*  */}
-      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md">
+      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-125">
         <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25"></div>
         <div className="flex justify-center w-10/12">
           <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
