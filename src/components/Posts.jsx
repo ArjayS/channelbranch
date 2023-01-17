346bc17b893400b0d99238bd3d52cae1edf3d3b6
Robert Servado <servadorobert@gmail.com>
2023-01-17 11:51:38 -0500

    Modified Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,14 +4,16 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
+      {/*  */}
       <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md">
+        <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25"></div>
         <div className="flex justify-center w-10/12">
           <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
         </div>
         <div className="bg-slate-200 rounded-full p-2 mr-1 bg-opacity-25">
           <form className="flex justify-around">
             <img
-              src={require("../imgs/Copy_logo1.svg").default}
+              src={require("../imgs/Copy_logo3.svg").default}
               alt="Copy Logo"
               type="submit"
               formAction=""
