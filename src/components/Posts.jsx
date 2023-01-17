805ea25081c2f256d2f7ae96de82ebf738fcd2e0
Robert Servado <servadorobert@gmail.com>
2023-01-16 20:45:27 -0500

    Modified Posts.jsx to add appropriate max-width

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,8 +4,13 @@ const Posts = (props) => {
   const { title } = props;
   return (
     <>
-      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md">
-        <h3 className="text-lg font-medium ml-4">Sample Post Title!</h3>
+      <div className="bg-white rounded-full p-2 mt-3 cursor-pointer flex flex-row justify-between items-center w-full bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md">
+        <div className="flex w-10/12">
+          <h3 className="text-lg font-medium ml-4">
+            Sample Post
+            Title!sfewfwefgwagawegawegwaegwgewegwgewagewagewegwgwgwgwageawgewaegwagewagwgawegwegwagwag
+          </h3>
+        </div>
         <div className="bg-slate-200 rounded-full p-2 mr-1">
           <form className="flex justify-around">
             <p></p>
