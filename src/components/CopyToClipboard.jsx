f9f1d7e3335e3578e3565115c563491d9282ee7a
Robert Servado <servadorobert@gmail.com>
2023-01-18 18:26:15 -0500

    Modified CopyToClipboard.jsx to have the clipboard image and setup

--- src/components/CopyToClipboard.jsx
+++ src/components/CopyToClipboard.jsx
@@ -2,7 +2,23 @@ import React, { useState } from "react";
 
 const CopyToClipboard = (props) => {
   const {} = props;
-  return <div></div>;
+  return (
+    <>
+      <div className="rounded-md hover:ring-2 ring-slate-400 mr-1 transform transition duration-100">
+        <div className="rounded-full bg-opacity-75 hover-target-fast p-1">
+          <form className="flex justify-around">
+            <img
+              src={require("../imgs/Copy_logo2.svg").default}
+              alt="Copy Logo"
+              type="submit"
+              formAction=""
+              className="h-4 w-4"
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CopyToClipboard;
