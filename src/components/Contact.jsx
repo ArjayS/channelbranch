29a9cfcee266a004318f402893f0596a56b7f9f5
Robert Servado <servadorobert@gmail.com>
2023-01-19 17:46:12 -0500

    Modifief Contact.jsx to add further styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
   const [status, setStatus] = useState({});
 
   return (
-    <div className="flex flex-col justify-center w-9/12">
+    <div className="flex flex-col justify-center w-11/12">
       {/* OLD DESIGN */}
       {/* <form
         action="https://formsubmit.co/servadorobert@gmail.com"
@@ -29,59 +29,70 @@ const Contact = () => {
       {/* OLD DESIGN */}
       <form>
         <div className="flex flex-row justify-between mt-4">
-          <div className="w-full">
-            <label
-              for="text"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
+          <div className="w-full mr-4">
+            <div className="flex justify-between ml-4">
+              <label
+                for="text"
+                className="block text-md font-medium text-gray-700"
+              >
+                Name
+              </label>
+            </div>
             <div className="relative mt-1 rounded-md shadow-sm">
               <input
                 type="text"
                 name="name"
                 id="name"
-                className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mt-2"
                 placeholder="Your Name"
                 required
               />
             </div>
           </div>
-          <div className="w-full">
-            <label
-              for="text"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
+          <div className="w-full ml-4">
+            <div className="flex justify-between ml-4">
+              <label
+                for="text"
+                className="block text-md font-medium text-gray-700"
+              >
+                Email
+              </label>
+            </div>
             <div className="relative mt-1 rounded-md shadow-sm">
               <input
                 type="email"
                 name="email"
                 id="email"
-                className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="Your Name"
+                className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mt-2"
+                placeholder="Your Email"
                 required
               />
             </div>
           </div>
         </div>
         <div className="mt-4">
-          <label for="text" className="block text-sm font-medium text-gray-700">
-            Message
-          </label>
+          <div className="flex justify-between ml-4">
+            <label
+              for="text"
+              className="block text-md font-medium text-gray-700 "
+            >
+              Message
+            </label>
+          </div>
           <div className="relative mt-1 rounded-md shadow-sm">
             <input
               type="text"
               name="message"
               id="message"
-              className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="block w-full rounded-md border-gray-300 pl-2 pr-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mt-2"
               placeholder="Your Message"
               required
             />
           </div>
         </div>
-        <button type="submit">Send</button>
+        <button type="submit" className="mt-4">
+          Send
+        </button>
       </form>
     </div>
   );
