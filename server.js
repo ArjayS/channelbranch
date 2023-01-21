a1e5e6c4834c6dec0820c85e06e4ee3feda1df70
Robert Servado <servadorobert@gmail.com>
2023-01-20 20:54:13 -0500

    Modified server.js

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const router = express.Router();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+app.listen(3001, () => console.log("Server is running"));
