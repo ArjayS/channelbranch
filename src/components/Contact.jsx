5468f26f9628fc4da3bda60381aa6d41132c18c0
Robert Servado <servadorobert@gmail.com>
2023-01-21 22:52:42 -0500

    Remove the old format of the formsubmit backend API

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,18 +26,6 @@ const Contact = () => {
 
   return (
     <div className="flex flex-col justify-center w-11/12">
-      {/* OLD DESIGN */}
-      {/* <form
-        action="https://formsubmit.co/servadorobert@gmail.com"
-        method="POST"
-        className="mt-3"
-      >
-        <input type="text" name="name" placeholder="Your Name" required />
-        <input type="email" name="email" placeholder="Your Email" required />
-        <input type="text" name="message" placeholder="Your Message" required />
-        <button type="submit">Send</button>
-      </form> */}
-      {/* OLD DESIGN */}
       <form
         // onSubmit={handleSubmit}
         action="https://formsubmit.co/servadorobert@gmail.com"
