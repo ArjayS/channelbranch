8c714df30980805ed0ac6e98c77072889c4c3e87
Robert Servado <servadorobert@gmail.com>
2023-01-25 20:47:56 -0500

    Modified DayNightToggle.jsx

--- src/components/DayNightToggle.jsx
+++ src/components/DayNightToggle.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const DayNightToggle = () => {
+const DayNightToggle = (props) => {
   const [theme, setTheme] = useState(null);
 
   useEffect(() => {
