f1b32aceb6036063666140e7942c9e3fdcbf8b7b
Robert Servado <servadorobert@gmail.com>
2023-01-31 18:35:12 -0500

    Modified Posts.jsx and changed the styling

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -19,7 +19,9 @@ const Posts = (props) => {
             </button>
           </form>
         </div>
-        <CopyToClipboard url={url} />
+        <div className="hover-target-fast">
+          <CopyToClipboard url={url} />
+        </div>
         {/*
         rounded-md hover:ring-2 ring-slate-400 mr-1 transform transition duration-100">
           <div className="rounded-full bg-opacity-75 hover-target-fast p-1">
