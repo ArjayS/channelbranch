c598d5d25d957d27ba25ebd57c6fe86fd4a81671
Robert Servado <servadorobert@gmail.com>
2023-01-31 18:03:40 -0500

    Moidifed App.js

--- src/App.js
+++ src/App.js
@@ -24,8 +24,11 @@ function App() {
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <main className="flex flex-col text-center items-center" id={theme}>
-        <header className="bg-white rounded-lg p-1 cursor-pointer flex flex-row justify-between items-center w-11/12 bg-opacity-50 backdrop-filter backdrop-blur-md max-w-screen-md hover:scale-110 hover-trigger transform transition duration-300">
+      <main
+        className="flex flex-col h-screen w-screen text-center items-center"
+        id={theme}
+      >
+        <header className="bg-white rounded-lg cursor-pointer flex flex-row w-6/12 justify-between items-center bg-opacity-50 w-">
           <Socials />
           <DayNightToggle
             onChange={toggleTheme}
@@ -33,7 +36,7 @@ function App() {
             theme={theme}
           />
         </header>
-        <div className="flex flex-col h-screen justify-between">
+        <div className="flex flex-col h-screen w-screen justify-between">
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<MainPage />} />
