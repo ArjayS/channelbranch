3a78c81fab5ea12d217370d144bc5003cb7bd624
Robert Servado <servadorobert@gmail.com>
2023-01-31 18:34:45 -0500

    Modified CopyToClipboard.jsx styling

--- src/components/CopyToClipboard.jsx
+++ src/components/CopyToClipboard.jsx
@@ -24,7 +24,7 @@ const CopyToClipboard = (props) => {
   return (
     <>
       <div className="rounded-md hover:ring-2 ring-slate-400 mr-1 transform transition duration-100">
-        <div className="rounded-full bg-opacity-75 hover-target-fast p-1">
+        <div className="rounded-full bg-opacity-75 p-1">
           {copied ? (
             <svg
               fill="none"
