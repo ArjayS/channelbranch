6d72fd3b47fd47c7f18e82e00edc903781329a4a
Robert Servado <servadorobert@gmail.com>
2023-02-02 15:21:59 -0500

    Modified tailwind.config.js and added media queries for different screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xsm: "400px",
+      // => @media (min-width: 400px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "820px",
+      // => @media (min-width: 820px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
