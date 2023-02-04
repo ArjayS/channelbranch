8c7e5043827840bbd509ff29bb3a22bf87a77e07
Robert Servado <servadorobert@gmail.com>
2023-02-03 19:03:02 -0500

    Modified Header.jsx and added a few more responsive design on the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <div className="flex flex-col items-center">
-      <header className="cursor-pointer flex flex-row w-4/12 justify-between items-center bg-opacity-50 mt-5">
+      <header className="cursor-pointer flex flex-row xsm:w-10/12 sm:w-10/12 md:w-10/12 lg:w-7/12 xl:w-7/12 2xl:w-6/12 3xl:w-4/12 4xl:w-3/12 justify-between items-center bg-opacity-50 mt-5">
         <div className="bg-slate-200 rounded-md bg-opacity-25">
           <CopyToClipboard
             url={"https://localhost:3000"}
