9190bded4f2584c02cb3f041e7e1b71c6dd2c423
Robert Servado <servadorobert@gmail.com>
2023-02-05 16:41:08 -0500

    Added ShareLinks.jsx to the proper component/ directory

---
+++ src/components/ShareLinks.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ShareLinks = () => {
+  return (
+    <>
+      <div>
+        <h1>ShareLink</h1>
+        <button>CLICK ME</button>
+      </div>
+    </>
+  );
+};
+
+export default ShareLinks;
